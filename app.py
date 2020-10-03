7513036c9af24e3c29851f0b21b0a09635c46e85
MicahJank <MicahJank@users.noreply.github.com>
2020-10-03 10:44:46 -0500

    sqlite database created

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 # tells our app where the database is located - using sqlite and the database will be named test and should be located in the project folder
-app.config['SQLALCHEMY_DATBASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 # initialize the database
 db = SQLAlchemy(app) 
+
+# this is similar to the migration files in knex - i am using this class to make the model of the todo
+# notice i am giving it an id and then creating the content column as well
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # everytime a todo is created we will return an element with the task id
+    def __repr__(self):
+        return f"<Task {self.id}>"
 
 @app.route('/')
 def index():
