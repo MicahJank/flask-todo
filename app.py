89847065b13f419e4beb4449ca4a69fdd25cf46c
MicahJank <MicahJank@users.noreply.github.com>
2020-10-02 22:07:51 -0500

    started to work on adding the sqlite database

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# tells our app where the database is located - using sqlite and the database will be named test and should be located in the project folder
+app.config['SQLALCHEMY_DATBASE_URI'] = 'sqlite:///test.db'
+
+# initialize the database
+db = SQLAlchemy(app) 
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
